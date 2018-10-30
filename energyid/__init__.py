c8f9c2c503bed84ad440fda7304856208489cc87
Jan Pecinovsky <janpecinovsky@gmail.com>
2018-10-30 23:38:54 +0100

    first bunch of methods

---
+++ energyid/__init__.py
@@ -0,0 +1,6 @@
+from .client import JSONClient
+
+__title__ = "energyid"
+__version__ = "0.0.1"
+__author__ = "EnergieID.be"
+__license__ = "MIT"
---
+++ energyid/client.py
@@ -0,0 +1,118 @@
+import requests
+from typing import Union, Optional
+
+URL = "https://api.energyid.eu/api/v1"
+
+class JSONClient:
+    def __init__(self, token: str):
+        self.session = requests.Session()
+        self.session.headers.update({"Authorization": f"bearer {token}"})
+
+    def _request(self, method: str, endpoint: str, **kwargs) -> dict:
+        url = f'{URL}/{endpoint}'
+        if method == 'GET':
+            r = self.session.get(url, params=kwargs)
+        elif method == 'POST':
+            r = self.session.post(url, data=kwargs)
+        elif method == 'PUT':
+            r = self.session.put(url, data=kwargs)
+        elif method == 'DELETE':
+            r = self.session.delete(url)
+            r.raise_for_status()
+            return {}
+        else:
+            raise ValueError(f'Unknown method: {method}')
+        r.raise_for_status()
+        j = r.json()
+        return j
+
+    def get_meter_catalog(self) -> dict:
+        endpoint = 'catalogs/meters'
+        return self._request('GET', endpoint)
+
+    def get_dataset_catalog(self) -> dict:
+        endpoint = 'catalogs/datasets'
+        return self._request('GET', endpoint)
+
+    def get_group(self, group_id: str, **kwargs) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}'
+        return self._request('GET', endpoint, **kwargs)
+
+    def get_group_languages(self, group_id: str) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/languages'
+        return self._request('GET', endpoint)
+
+    def get_group_records(self, group_id: str, top: int=200, skip: int=0) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/records'
+        return self._request('GET', endpoint, top=top, skip=skip)
+
+    def get_group_members(self, group_id: str, top: int=200, skip: int=0) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'group/{group_id}/members'
+        return self._request('GET', endpoint, top=top, skip=skip)
+
+    def get_records_for_group_member(self, group_id: str, user_id: str) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/members/{user_id}/records'
+        return self._request('GET', endpoint)
+
+    def get_group_membership_details(self, group_id: str, record_id: str) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/records/{record_id}'
+        return self._request('GET', endpoint)
+
+    def add_record_to_group(self, group_id: str, record_number: str, identification_key: Optional[str]=None) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/records'
+        return self._request('POST', endpoint, RecordNumber=record_number, IdentificationKey=identification_key)
+
+    def change_reference_of_record_in_group(self, group_id: str, record_id: str, reference: Optional[str]=None) -> dict:
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/records/{record_id}/reference'
+        return self._request('PUT', endpoint, newReference=reference)
+
+    def remove_record_from_group(self, group_id: str, record_id: str):
+        """group_id can also be the group slug"""
+        endpoint = f'groups/{group_id}/records/{record_id}'
+        self._request('DELETE', endpoint)
+
+    def get_member(self, user_id: str='me') -> dict:
+        """user_id can also be an e-mail address or simply 'me'"""
+        endpoint = f'members/{user_id}'
+        return self._request('GET', endpoint)
+
+    def get_member_groups(self, user_id: str='me', **kwargs) -> dict:
+        """user_id can also be an e-mail address or simply 'me'"""
+        endpoint = f'members/{user_id}/groups'
+        return self._request('GET', endpoint, **kwargs)
+
+    def get_member_records(self, user_id: str='me') -> dict:
+        """user_id can also be an e-mail address or simply 'me'"""
+        endpoint = f'members/{user_id}/records'
+        return self._request('GET', endpoint)
+
+    def get_meter(self, meter_id: str) -> dict:
+        endpoint = f'meters/{meter_id}'
+        return self._request('GET', endpoint)
+
+    def get_meter_readings(self, meter_id: str, take: int=1000, skip: int=0, next_key: Optional[str]=None) -> dict:
+        endpoint = f'meters/{meter_id}/readings'
+        return self._request('GET', endpoint, take=take, skip=skip, nextRowKey=next_key)
+
+    def get_meter_latest_reading(self, meter_id: str) -> dict:
+        endpoint = f'meters/{meter_id}/readings/latest'
+        return self._request('GET', endpoint)
+
+    def create_meter_reading(self, meter_id: str, value: Union[int, float], timestamp: Optional[str]=None) -> dict:
+        endpoint = f'meters/{meter_id}/readings'
+        return self._request('POST', endpoint, value=value, timestamp=timestamp)
+
+    def create_meter(self, record_number: str, display_name: str, metric: str, unit: str, reading_type: str,
+                     multiplier: Union[int, float]=1, **kwargs):
+        endpoint = f'meters'
+        return self._request('POST', endpoint, recordNumber=record_number, displayName=display_name, metric=metric,
+                             unit=unit, readingType=reading_type, multiplier=multiplier, **kwargs)
+
