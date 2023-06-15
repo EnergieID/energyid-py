b9e704fe617e9665c0752f01a3e212e7d1282692
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2023-06-15 12:04:59 +0200

    version bump

--- energyid/__init__.py
+++ energyid/__init__.py
@@ -2,6 +2,6 @@ from .client import JSONClient, SimpleJSONClient, Scope
 from .pandasclient import PandasClient
 
 __title__ = "energyid"
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 __author__ = "EnergieID.be"
 __license__ = "MIT"
